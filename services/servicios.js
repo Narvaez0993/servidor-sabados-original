1cd6710ab03b5f018f55f4227424f3b7016d1998
Sebastian Mejía <santiagogonzalez@cs-re-bb-jf.administrativo.cesde.edu.co>
2021-10-30 15:47:12 -0500

    subo el servidor

---
+++ services/servicios.js
@@ -0,0 +1,50 @@
+//importo el modelo de datos
+const JugadorModelo = require('../models/JugadorModelo.js')
+
+//Servicio para agregar un documento a la bd
+async function insertarJugador(datosJugador){
+
+    let jugadorAInsertar=new JugadorModelo(datosJugador)
+
+    return await jugadorAInsertar.save()
+
+}
+
+//servicio para buscar 1 documento de la bd
+
+async function leerJugador(id){
+
+    let jugador = await JugadorModelo.findById(id)
+    return jugador
+}
+
+
+
+//servicio para buscar Todos los documentos de
+
+async function leerJugadores(){
+    let jugadores=await JugadorModelo.find();
+    return jugadores
+}
+
+//servicio para editar un documentos
+
+async function modificarJugador(id,datos){
+    return await JugadorModelo.findByIdAndUpdate(id,datos)
+}
+
+
+//servicio para eliminar un documento
+
+async function borrarJugador(id){
+    return await JugadorModelo.findByIdAndDelete(id)
+}
+
+module.exports={
+    insertarJugador,
+    leerJugador,
+    leerJugadores,
+    modificarJugador,
+    borrarJugador
+}
+
